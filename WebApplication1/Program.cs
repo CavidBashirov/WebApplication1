c526fb03dc99e9bf6f2d3d59174e16605dcd6f2d
Cavid Bashirov <bashirovcavid93gmail.com>
2024-12-11 13:16:31 +0400

    changed

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -15,11 +15,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-else
-{
+
     var port = Environment.GetEnvironmentVariable("PORT") ?? "3000";
     app.Urls.Add($"http://0.0.0.0:{port}");
-}
+
 
 
 
